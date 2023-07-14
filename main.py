444c3404d2a9ab1cbfa5319d7c263a3ccc4077cd
Dejan1990 <dejan.simo@hotmail.com>
2023-07-14 20:28:37 +0200

    Add random library

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-def get_choices():
-    player_choice = input("Enter a choice (rock, paper, scissors: ")
-    computer_choice = "paper"
-    choices = {"player": player_choice, "computer": computer_choice}
+import random
+
+#def get_choices():
+#    player_choice = input("Enter a choice (rock, paper, scissors: ")
+#    computer_choice = "paper"
+#    choices = {"player": player_choice, "computer": computer_choice}
     
-    return choices
+#    return choices
 
 
-choices = get_choices()
-print(choices)
+#choices = get_choices()
+#print(choices)
+
+
+food = ['eggs', 'carrots', 'pizza']
+dinner = random.choice(food)
+print(dinner)
