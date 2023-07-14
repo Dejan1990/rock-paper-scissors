ac14accefd63203d75fdd8b852fdfcb5bd3c5b11
Dejan1990 <dejan.simo@hotmail.com>
2023-07-14 21:45:48 +0200

    Concatenating string

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import random
 
 def get_choices():
     options = ['rock', 'paper', 'scissors']
-    user_choice = input("Enter a choice (paper, rock, scissors): ")
+    player_choice = input("Enter a choice (paper, rock, scissors): ")
     computer_choice = random.choice(options)
-    choices = {"user": user_choice, "computer": computer_choice}
+    choices = {"user": player_choice, "computer": computer_choice}
     
     return choices
 
-choice = get_choices()
-print(choice)
+def check_win(player, computer):
+    print("You chose " + player + ", Computer chose " + computer)
+    if player == computer:
+        return "It's a tie!"
+
+
