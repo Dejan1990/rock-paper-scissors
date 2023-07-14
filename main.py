828f3ed3105405bf08037f6962a22ce07a7dd38b
Dejan1990 <dejan.simo@hotmail.com>
2023-07-14 20:09:27 +0200

    Dictionaries

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def get_choices():
     player_choice = "rock"
     computer_choice = "paper"
+    choices = {"player": player_choice, "computer": computer_choice}
     
-    return computer_choice
+    return choices
 
 
 choices = get_choices()
