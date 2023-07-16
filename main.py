cf7743da3b85f297387507f34ae52bca53392ccb
Dejan1990 <dejan.simo@hotmail.com>
2023-07-16 12:17:27 +0200

    Elif statements

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ def get_choices():
     options = ['rock', 'paper', 'scissors']
     player_choice = input("Enter a choice (paper, rock, scissors): ")
     computer_choice = random.choice(options)
-    choices = {"user": player_choice, "computer": computer_choice}
+    choices = {"player": player_choice, "computer": computer_choice}
     
     return choices
 
 def check_win(player, computer):
     #print("You chose " + player + ", Computer chose " + computer)
-    print(f"You chose {player}, Computer chose {computer}")
+    print(f"You choose {player} and computer choose {computer}")
     if player == computer:
         return "It's a tie!"
+    elif player == "rock" and computer == "scissors":
+        return "Rock smashed scissors! You win!"
+    elif player == "rock" and computer == "paper":
+        return "Paper covers rock! You lose."
+    elif player == "paper" and computer == "rock":
+        return "Paper covers rock! You win!" 
+    elif player == "paper" and computer == "scissors":
+        return "Scissors cuts paper! You lose."
+    elif player == "scissors" and computer == "paper":
+        return "Scissors cuts paper! You win!"
+    elif player == "scissors" and computer == "rock":
+        return "Rock smashed scissors! You lose."
     
-check_win('rock', 'rock')
+check_win("rock", "scissors")
 
 
