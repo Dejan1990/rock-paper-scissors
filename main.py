138f78511367390e3bf557c09f311966293b1b1a
Dejan1990 <dejan.simo@hotmail.com>
2023-07-16 13:10:57 +0200

    Add if statement to check input()

--- main.py
+++ main.py
@@ -10,23 +10,27 @@ def get_choices():
 
 def check_win(player, computer):
   print(f"You chose {player}, computer chose {computer}")
-  if player == computer:
-    return "It's a tie!"
-  elif player == "rock":
-    if computer == "scissors":
-      return "Rock smashes scissors! You win!"
-    else:
-      return "Paper covers rock! You lose."
-  elif player == "paper":
-    if computer == "rock":
-      return "Paper covers rock! You win!"
-    else:
-      return "Scissors cuts paper! You lose."
-  elif player == "scissors":
-    if computer == "paper":
-      return "Scissors cuts paper! You win!"
-    else:
-      return "Rock smashes scissors! You lose."
+  if player == "rock" or player == "paper" or player == "scissors":
+    
+    if player == computer:
+        return "It's a tie!"
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock smashes scissors! You win!"
+        else:
+            return "Paper covers rock! You lose."
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers rock! You win!"
+        else:
+            return "Scissors cuts paper! You lose."
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cuts paper! You win!"
+        else:
+            return "Rock smashes scissors! You lose."
+  else:
+      return "You must chose rock, paper or scissors, nothing else"
   
 choices = get_choices()
 result = check_win(choices["player"], choices["computer"])
