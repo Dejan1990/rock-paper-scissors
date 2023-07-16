526d60ff39cfb86b677508d4b4261c13d6163a01
Dejan1990 <dejan.simo@hotmail.com>
2023-07-16 12:55:54 +0200

    Refactoring and nested if

--- main.py
+++ main.py
@@ -9,23 +9,25 @@ def get_choices():
     return choices
 
 def check_win(player, computer):
-    #print("You chose " + player + ", Computer chose " + computer)
-    print(f"You choose {player} and computer choose {computer}")
-    if player == computer:
-        return "It's a tie!"
-    elif player == "rock" and computer == "scissors":
-        return "Rock smashed scissors! You win!"
-    elif player == "rock" and computer == "paper":
-        return "Paper covers rock! You lose."
-    elif player == "paper" and computer == "rock":
-        return "Paper covers rock! You win!" 
-    elif player == "paper" and computer == "scissors":
-        return "Scissors cuts paper! You lose."
-    elif player == "scissors" and computer == "paper":
-        return "Scissors cuts paper! You win!"
-    elif player == "scissors" and computer == "rock":
-        return "Rock smashed scissors! You lose."
+  print(f"You chose {player}, computer chose {computer}")
+  if player == computer:
+    return "It's a tie!"
+  elif player == "rock":
+    if computer == "scissors":
+      return "Rock smashes scissors! You win!"
+    else:
+      return "Paper covers rock! You lose."
+  elif player == "paper":
+    if computer == "rock":
+      return "Paper covers rock! You win!"
+    else:
+      return "Scissors cuts paper! You lose."
+  elif player == "scissors":
+    if computer == "paper":
+      return "Scissors cuts paper! You win!"
+    else:
+      return "Rock smashes scissors! You lose."
     
-check_win("rock", "scissors")
+check_win("rock", "paper")
 
 
